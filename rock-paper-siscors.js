fb41660b1818ed9074a3671490df0ba493d42728
sumstot <soren.umstot@gmail.com>
2022-08-11 15:50:44 +0900

    fixed my stupid spelling mistakes

--- rock-paper-siscors.js
+++ rock-paper-siscors.js
@@ -1,5 +1,5 @@
 function getComputerChoice () {
-  var choice = ["rock", "paper", "sissors"]
+  var choice = ["rock", "paper", "scissors"]
   var randomNumber = Math.floor(Math.random()*choice.length)
   return choice[randomNumber]
 }
@@ -10,14 +10,14 @@ for (let i = 0; i < 5; i++){
 
 var comp_selection = getComputerChoice()
 
-player_selection = prompt("Please choose: Rock, Paper, or Sissors").toLowerCase();
+player_selection = prompt("Please choose: Rock, Paper, or Scissors").toLowerCase();
 
 function rps_game(player_selection, comp_selection) {
 if (player_selection === comp_selection) {
   console.log(`It's a tie. You chose ${player_selection}, computer chose ${comp_selection}.`)
   console.log(`Player Score: ${player_score}  Computer Score ${comp_score}`)
 }
-else if (player_selection === "rock" && comp_selection === "sissors" || player_selection === "sissors" && comp_selection === "paper" || player_selection === "paper" && comp_selection === "rock"){
+else if (player_selection === "rock" && comp_selection === "scissors" || player_selection === "scissors" && comp_selection === "paper" || player_selection === "paper" && comp_selection === "rock"){
   player_score += 1
   console.log(`You win! You chose ${player_selection}, computer chose ${comp_selection}.`)
   console.log(`Player Score: ${player_score}  Computer Score ${comp_score}`)
