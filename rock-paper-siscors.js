cd921f2274921051318819ec6a4df1eaacfb69c2
sumstot <soren.umstot@gmail.com>
2022-08-11 11:46:40 +0900

    this is the first version of my rock paper sissors game

--- rock-paper-siscors.js
+++ rock-paper-siscors.js
@@ -3,5 +3,25 @@ function getComputerChoice () {
   var randomNumber = Math.floor(Math.random()*choice.length)
   return choice[randomNumber]
 }
-var comp_answer = getComputerChoice()
-console.log(comp_answer)
+var comp_selection = getComputerChoice()
+console.log(comp_selection)
+
+player_selection = prompt("Please choose: Rock, Paper, or Sissors").toLowerCase();
+
+function rps_game(player_selection, comp_selection) {
+if (player_selection === comp_selection) {
+  console.log(`It's a tie. You chose ${player_selection}, computer chose ${comp_selection}.`)
+}
+else if (player_selection === "rock" && comp_selection === "sissors" || player_selection === "sissors" && comp_selection === "paper" || player_selection === "paper" && comp_selection === "rock"){
+  console.log(`You win! You chose ${player_selection}, computer chose ${comp_selection}.`)
+}
+
+else{
+  console.log(`You lose. You chose ${player_selection}, computer chose ${comp_selection}.`)
+}
+}
+
+rps_game(player_selection, comp_selection)
+// for (let i = 0; i < 5; i++){
+//   rps_game
+// }
