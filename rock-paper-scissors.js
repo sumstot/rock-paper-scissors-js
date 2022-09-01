8d47112494295f6e269709e6cafb59df6067fa00
sumstot <soren.umstot@gmail.com>
2022-09-01 15:38:30 +0900

    Added event listeners for clicks that alerts ID. Need to find out how to use event listener for player choice. CSS custom fonts added. Styling is solid

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,45 +1,47 @@
+// function for computer choice
 function getComputerChoice() {
-  var choice = ["rock", "paper", "scissors"];
-  var randomNumber = Math.floor(Math.random() * choice.length);
+  let choice = ["rock", "paper", "scissors"];
+  let randomNumber = Math.floor(Math.random() * choice.length);
   return choice[randomNumber];
 }
+// variables for score
+let player_score = 0;
+let comp_score = 0;
 
-var player_score = 0;
-var comp_score = 0;
+//event listener for player choice
+const buttons = document.querySelectorAll("button");
+let playerChoice = buttons.forEach((button) => {
+  button.addEventListener("click", function handleClick(event) {
+    alert(event.target.parentElement.id);
+  });
+});
 
+// const playerChoice = "rock";
 // for (let i = 0; i < 5; i++){
 
-var comp_selection = getComputerChoice();
+let comp_selection = getComputerChoice();
 
-player_selection = prompt(
-  "Please choose: Rock, Paper, or Scissors"
-).toLowerCase();
+//TODO this is for testing
+// player_selection = prompt(
+//   "Please choose: Rock, Paper, or Scissors"
+// ).toLowerCase();
 
 function rps_game(player_selection, comp_selection) {
   if (player_selection === comp_selection) {
-    console.log(
-      `It's a tie. You chose ${player_selection}, computer chose ${comp_selection}.`
-    );
-    console.log(`Player Score: ${player_score}  Computer Score ${comp_score}`);
+    return `It's a tie. You chose ${player_selection}, computer chose ${comp_selection}.`;
   } else if (
     (player_selection === "rock" && comp_selection === "scissors") ||
     (player_selection === "scissors" && comp_selection === "paper") ||
     (player_selection === "paper" && comp_selection === "rock")
   ) {
     player_score += 1;
-    console.log(
-      `You win! You chose ${player_selection}, computer chose ${comp_selection}.`
-    );
-    console.log(`Player Score: ${player_score}  Computer Score ${comp_score}`);
+    return `You win! You chose ${player_selection}, computer chose ${comp_selection}.`;
   } else {
     comp_score += 1;
-    console.log(
-      `You lose. You chose ${player_selection}, computer chose ${comp_selection}.`
-    );
-    console.log(`Player Score: ${player_score}  Computer Score ${comp_score}`);
+    return `You lose. You chose ${player_selection}, computer chose ${comp_selection}.`;
   }
 }
-rps_game(player_selection, comp_selection);
+let rpsResult = rps_game(playerChoice, comp_selection);
 // }
 
 if (player_score > comp_score) {
@@ -55,9 +57,31 @@ if (player_score > comp_score) {
     `Final Score Player: ${player_score} Computer: ${comp_score}. You lose.`
   );
 }
+//TODO remove and reappend element after a round
+// deals with the scoring of the game in DOM
+const score = document.querySelector("#score");
 
-const rockButton = document.querySelector("rock");
-const paperButton = document.querySelector("paper");
-const scissors = document.querySelector("scissors");
+const h1PlayerScore = document.createElement("h3");
+h1PlayerScore.textContent = `Player's current score: ${player_score}`;
+h1PlayerScore.style.color = "white";
+h1PlayerScore.style.textAlign = "center";
 
-rockButton.addEventListener("click", (e) => console.log(e));
+const h1CompScore = document.createElement("h3");
+h1CompScore.textContent = `Computer's current score: ${comp_score}`;
+h1CompScore.style.textAlign = "center";
+h1CompScore.style.color = "white";
+
+score.appendChild(h1PlayerScore);
+score.appendChild(h1CompScore);
+
+//TODO remove and reappend element after a round
+// deals with posting the results of the game in DOM
+const result = document.querySelector("#result");
+
+const gameResult = document.createElement("h3");
+gameResult.style.color = "white";
+gameResult.style.textAlign = "center";
+gameResult.textContent = `${rpsResult}`;
+result.appendChild(gameResult);
+
+//event listeners for button clicks
