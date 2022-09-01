664b26f0c981ff92de54b2af77f5d98e60d5b35d
sumstot <soren.umstot@gmail.com>
2022-09-01 11:25:12 +0900

    This is the html framework for my rock paper scissors game. The JS code has not been updated yet

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,45 +1,63 @@
-function getComputerChoice () {
-  var choice = ["rock", "paper", "scissors"]
-  var randomNumber = Math.floor(Math.random()*choice.length)
-  return choice[randomNumber]
+function getComputerChoice() {
+  var choice = ["rock", "paper", "scissors"];
+  var randomNumber = Math.floor(Math.random() * choice.length);
+  return choice[randomNumber];
 }
-var player_score = 0
-var comp_score = 0
 
-for (let i = 0; i < 5; i++){
+var player_score = 0;
+var comp_score = 0;
 
-var comp_selection = getComputerChoice()
+// for (let i = 0; i < 5; i++){
 
-player_selection = prompt("Please choose: Rock, Paper, or Scissors").toLowerCase();
+var comp_selection = getComputerChoice();
+
+player_selection = prompt(
+  "Please choose: Rock, Paper, or Scissors"
+).toLowerCase();
 
 function rps_game(player_selection, comp_selection) {
-if (player_selection === comp_selection) {
-  console.log(`It's a tie. You chose ${player_selection}, computer chose ${comp_selection}.`)
-  console.log(`Player Score: ${player_score}  Computer Score ${comp_score}`)
+  if (player_selection === comp_selection) {
+    console.log(
+      `It's a tie. You chose ${player_selection}, computer chose ${comp_selection}.`
+    );
+    console.log(`Player Score: ${player_score}  Computer Score ${comp_score}`);
+  } else if (
+    (player_selection === "rock" && comp_selection === "scissors") ||
+    (player_selection === "scissors" && comp_selection === "paper") ||
+    (player_selection === "paper" && comp_selection === "rock")
+  ) {
+    player_score += 1;
+    console.log(
+      `You win! You chose ${player_selection}, computer chose ${comp_selection}.`
+    );
+    console.log(`Player Score: ${player_score}  Computer Score ${comp_score}`);
+  } else {
+    comp_score += 1;
+    console.log(
+      `You lose. You chose ${player_selection}, computer chose ${comp_selection}.`
+    );
+    console.log(`Player Score: ${player_score}  Computer Score ${comp_score}`);
+  }
 }
-else if (player_selection === "rock" && comp_selection === "scissors" || player_selection === "scissors" && comp_selection === "paper" || player_selection === "paper" && comp_selection === "rock"){
-  player_score += 1
-  console.log(`You win! You chose ${player_selection}, computer chose ${comp_selection}.`)
-  console.log(`Player Score: ${player_score}  Computer Score ${comp_score}`)
+rps_game(player_selection, comp_selection);
+// }
+
+if (player_score > comp_score) {
+  console.log(
+    `Final Score Player: ${player_score} Computer: ${comp_score}. You win!`
+  );
+} else if (player_score === comp_score) {
+  console.log(
+    `Final Score Player: ${player_score} Computer: ${comp_score}. It's a tie`
+  );
+} else {
+  console.log(
+    `Final Score Player: ${player_score} Computer: ${comp_score}. You lose.`
+  );
 }
 
-else{
-  comp_score += 1
-  console.log(`You lose. You chose ${player_selection}, computer chose ${comp_selection}.`)
-  console.log(`Player Score: ${player_score}  Computer Score ${comp_score}`)
-}
-}
-rps_game(player_selection, comp_selection)
-}
+const rockButton = document.querySelector("rock");
+const paperButton = document.querySelector("paper");
+const scissors = document.querySelector("scissors");
 
-if (player_score > comp_score){
-  console.log(`Final Score Player: ${player_score} Computer: ${comp_score}. You win!`)
-}
-
-else if (player_score === comp_score){
-  console.log(`Final Score Player: ${player_score} Computer: ${comp_score}. It's a tie`)
-}
-
-else{
-  console.log(`Final Score Player: ${player_score} Computer: ${comp_score}. You lose.`)
-}
+rockButton.addEventListener("click", (e) => console.log(e));
