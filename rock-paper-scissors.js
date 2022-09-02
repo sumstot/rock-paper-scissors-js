e6f9a77de1492019e4ef10750eafb75b08a559b6
sumstot <soren.umstot@gmail.com>
2022-09-02 14:51:54 +0900

    added scoring and result functionality. Event listeners on the buttons are finished. Need to fix a bug of results appending rather than replacing. Still need to add animations to rock paper scissors selections

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -7,84 +7,70 @@ function getComputerChoice() {
 // variables for score
 let player_score = 0;
 let comp_score = 0;
+const ROUNDS = 5;
+const score = document.querySelector("#score");
+const h1PlayerScore = document.createElement("h3");
+const h1CompScore = document.createElement("h3");
+const result = document.querySelector("#result");
 
-// const playerChoice = "rock";
+const getScore = function (player_score, comp_score) {
+  h1PlayerScore.textContent = `Player's current score: ${player_score}`;
+  h1PlayerScore.style.color = "white";
+  h1PlayerScore.style.textAlign = "center";
+  score.appendChild(h1PlayerScore);
+  h1CompScore.textContent = `Computer's current score: ${comp_score}`;
+  h1CompScore.style.textAlign = "center";
+  h1CompScore.style.color = "white";
+  score.appendChild(h1CompScore);
+};
+
+const getCompScore = function () {};
+
 // for (let i = 0; i < 5; i++){
-let comp_selection = getComputerChoice();
 
 //TODO this is for testing
-// player_selection = prompt(
-//   "Please choose: Rock, Paper, or Scissors"
-// ).toLowerCase();
 
-function rps_game(player_selection, comp_selection) {
+function getWinner(player_selection, comp_selection) {
   if (player_selection === comp_selection) {
-    return `It's a tie. You chose ${player_selection}, computer chose ${comp_selection}.`;
+    getScore(player_score, comp_score);
+    return `You both chose ${player_selection}. It's a tie 🤷‍♂️`;
   } else if (
     (player_selection === "rock" && comp_selection === "scissors") ||
     (player_selection === "scissors" && comp_selection === "paper") ||
     (player_selection === "paper" && comp_selection === "rock")
   ) {
     player_score += 1;
-    return `You win! You chose ${player_selection}, computer chose ${comp_selection}.`;
+    getScore(player_score, comp_score);
+    return `You chose ${player_selection}. Computer chose ${comp_selection}. YOU WIN 🎉`;
   } else {
     comp_score += 1;
-    return `You lose. You chose ${player_selection}, computer chose ${comp_selection}.`;
+    getScore(player_score, comp_score);
+    return `You chose ${player_selection}. Computer chose ${comp_selection}. You lose 😔`;
   }
 }
-let rpsResult = rps_game(player_selection, comp_selection);
 
-if (player_score > comp_score) {
-  console.log(
-    `Final Score Player: ${player_score} Computer: ${comp_score}. You win!`
-  );
-} else if (player_score === comp_score) {
-  console.log(
-    `Final Score Player: ${player_score} Computer: ${comp_score}. It's a tie`
-  );
-} else {
-  console.log(
-    `Final Score Player: ${player_score} Computer: ${comp_score}. You lose.`
-  );
+//Play game
+
+function play(e) {
+  const playerChoice = e.target.id;
+  const computerChoice = getComputerChoice();
+  const result = [];
+  result.push(playerChoice);
+  result.push(computerChoice);
+  console.log(result);
+  const winner = getWinner(playerChoice, computerChoice);
+  rpsResult(winner);
 }
-//TODO remove and reappend element after a round
-// deals with the scoring of the game in DOM
-const score = document.querySelector("#score");
 
-const h1PlayerScore = document.createElement("h3");
-h1PlayerScore.textContent = `Player's current score: ${player_score}`;
-h1PlayerScore.style.color = "white";
-h1PlayerScore.style.textAlign = "center";
-
-const h1CompScore = document.createElement("h3");
-h1CompScore.textContent = `Computer's current score: ${comp_score}`;
-h1CompScore.style.textAlign = "center";
-h1CompScore.style.color = "white";
-
-score.appendChild(h1PlayerScore);
-score.appendChild(h1CompScore);
-
-//TODO remove and reappend element after a round
-// deals with posting the results of the game in DOM
-const result = document.querySelector("#result");
-
-const gameResult = document.createElement("h3");
-gameResult.style.color = "white";
-gameResult.style.textAlign = "center";
-gameResult.textContent = `${rpsResult}`;
-result.appendChild(gameResult);
+const rpsResult = function (winner) {
+  const gameResult = document.createElement("h3");
+  gameResult.style.color = "white";
+  gameResult.style.textAlign = "center";
+  gameResult.textContent = `${winner}`;
+  result.appendChild(gameResult);
+};
 
 //event listeners for button clicks
-const rock = document.getElementById("rock");
-const paper = document.getElementById("paper");
-const scissors = document.getElementById("scissors");
 
-rock.addEventListener("click", function () {
-  rps_game("rock", getComputerChoice);
-});
-paper.addEventListener("click", function () {
-  rps_game("paper", getComputerChoice);
-});
-scissors.addEventListener("click", function () {
-  rps_game("scissors", getComputerChoice);
-});
+const choices = document.querySelectorAll(".choices");
+choices.forEach((choice) => choice.addEventListener("click", play));
